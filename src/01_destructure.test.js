7da21c9ba5cce98dabaa6b222dcb74e111fffe2a
Sam Jones <sam.jones494@googlemail.com>
2018-10-07 12:27:28 +0100

    destructuring - arrays

--- src/01_destructure.test.js
+++ src/01_destructure.test.js
@@ -4,21 +4,21 @@
 describe('destructuring arrays makes shorter code', () => {
 
   it('extract value from array, e.g. extract 0 into x like so `let [x] = [0]`', () => {
-    let firstValue = [1]
+    let [firstValue] = [1]
 
     expect(firstValue).toEqual(1)
   })
 
   it('leading commas', () => {
     const all = ['ax', 'why', 'zet']
-    const [,z] = all
+    const [,,z] = all
 
     expect(z).toEqual('zet')
   })
 
   it('extract from nested arrays', () => {
     const user = [['Some', 'One'], 23]
-    const [firstName, surname, age] = user
+    const [[firstName, surname], age] = user
 
     const expected = 'Some One = 23 years'
     expect(`${firstName} ${surname} = ${age} years`).toEqual(expected)
@@ -26,13 +26,13 @@ describe('destructuring arrays makes shorter code', () => {
 
   it('chained assignments', () => {
     let c, d
-    let a, b = [c, d] = [1, 2]
-
+    let [a, b] = [c, d] = [1, 2]
+    console.log(a)
     expect([a, b, c, d]).toEqual([1, 2, 1, 2])
   })
 
   it('in for-of loop', () => {
-    for (var [a, b] of [[0, 1, 2]]) {}
+    for (var [,a, b] of [[0, 1, 2]]) {}
     expect([a, b]).toEqual([1, 2])
   })
 

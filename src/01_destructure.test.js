66ec3b2e7295047c596cc04514b008c264df100d
Sam Jones <sam.jones494@googlemail.com>
2018-10-07 12:44:31 +0100

    destructuring - default args

--- src/01_destructure.test.js
+++ src/01_destructure.test.js
@@ -101,31 +101,31 @@ describe('destructuring objects', () => {
 describe('destructuring can also have default values', () => {
 
   it('for an empty array', () => {
-    const [a:1] = []
+    const [a = 1] = []
 
     expect(a).toEqual(1)
   })
 
   it('for a missing value', () => {
-    const [b=2] = [1,,3]
+    const [,b=2] = [1,,3]
 
     expect(b).toEqual(2)
   })
 
   it('in an object', () => {
-    const [a, b=2] = {a: 1}
+    const {a, b=2} = {a: 1}
 
     expect(b).toEqual(2)
   })
 
   it('if the value is undefined', () => {
-    const {a, b=[2]} = {a: 1, b: void 0}
+    const {a, b=2} = {a: 1, b: void 0}
 
     expect(b).toEqual(2)
   })
 
   it('also a string works with defaults', () => {
-    const [b=2] = '1'
+    const [a,b=2] = '1'
 
     expect(a).toEqual('1')
     expect(b).toEqual(2)
